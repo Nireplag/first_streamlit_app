d2cce0a27f06a0d0b34ee378f7b7028ce6d05e9d
Nireplag <70478646+Nireplag@users.noreply.github.com>
2023-02-02 23:18:32 -0300

    add fruit selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as stm
 import pandas as pd
 
+#import file
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 stm.title("My Parents New Healthy Diner")
 stm.header("Breakfast Favorites")
 stm.text("🥣 Omega 3 & Bluebarry Oatmeal")
@@ -9,6 +12,7 @@ stm.text("🐔 Hard-Boiled Free-Range Egg")
 stm.text('🥑🍞 Avocado Toast')
 
 stm.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+stm.multiselect('Pick some fruits:', list(my_fruit_list.Fruit))
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 stm.dataframe(my_fruit_list)
