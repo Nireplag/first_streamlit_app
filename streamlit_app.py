0bd6113cf1f36a16cca57e4bad181680cbf6c78f
Nireplag <70478646+Nireplag@users.noreply.github.com>
2023-02-02 23:02:57 -0300

    Update streamlit_app.py
    Add Header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as stm
 
 stm.title("My Parents New Healthy Diner")
+stm.header("Breakfast Menu")
+stm.text("Omega 3 & Bluebarry Oatmeal")
+stm.text("Kale, Spinach & Rocket Smoothie")
+stm.text("Hard-Boiled Free-Range Egg")
