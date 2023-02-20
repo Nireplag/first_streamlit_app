9b528d98cab43470c2390239b5b14c2b3809f097
Nireplag <70478646+Nireplag@users.noreply.github.com>
2023-02-19 22:42:53 -0300

    add fruit selector to the api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 stm.dataframe(fruits_to_show)
 
 stm.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #stm.text(fruityvice_response.json()) # write json format in screen
 
 # normalize json version of response
