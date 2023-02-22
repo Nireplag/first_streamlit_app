7f1fb5096012df55a53406787a579b8bea4037f9
Nireplag <70478646+Nireplag@users.noreply.github.com>
2023-02-21 21:43:58 -0300

    import snowflake connectors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as stm
 import pandas as pd
 import requests
+import snowflake.connector
 
 #import file
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
